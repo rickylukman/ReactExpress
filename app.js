0a71ad3feb6b5ada166f65bf4ea36c2e84ab98bd
rickylukman <rickylukman0606@gmail.com>
2021-03-14 23:08:27 +0700

    react express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+app = express()
+
+require('dotenv').config()
+app.set('view engine', 'ejs')
+app.use('/api/', require('./routes/hello'))
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, ()=> {
+    const url = `http://localhost:${PORT}/`
+    console.log(`Listening on Port: ${PORT}`)
+})
